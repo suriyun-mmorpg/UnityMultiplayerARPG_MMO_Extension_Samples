108fc3a0983ef1beeaf6ab84458c7a11cbbb3e15
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-05-22 15:52:47 +0700

    Add sample send client message to central server

---
+++ CustomFromClientToCentralServer/Scripts/CentralNetworkManager_Sample_CustomFromClientToCentralServer.cs
@@ -0,0 +1,32 @@
+﻿using LiteNetLibManager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG.MMO
+{
+    public partial class CentralNetworkManager
+    {
+        [DevExtMethods("RegisterServerMessages")]
+        private void CustomFromClientToCentralServer_RegisterServerMessages()
+        {
+            // Server will receive this message from map-server → `CentralAppServerRegister`, then send something back to client
+            RegisterServerMessage(10001, SampleHandleMsgFromMapServer);
+        }
+
+        // Map-Server message handlers
+        private void SampleHandleMsgFromMapServer(LiteNetLibMessageHandler messageHandler)
+        {
+            string text = messageHandler.reader.GetString();
+            float clientTime = messageHandler.reader.GetFloat();
+            long connectionId = messageHandler.reader.GetLong();
+            Debug.Log("[CustomFromClientToCentralServer] central-server receive: " + text + " " + clientTime + " then send lower-case text back to map-server");
+            ServerSendPacket(messageHandler.connectionId, LiteNetLib.DeliveryMethod.ReliableOrdered, 10001, (writer) =>
+            {
+                writer.Put(text.ToLower());
+                writer.Put(clientTime);
+                writer.Put(connectionId);
+            });
+        }
+    }
+}
---
+++ CustomFromClientToCentralServer/Scripts/MapNetworkManager_Sample_CustomFromClientToCentralServer.cs
@@ -0,0 +1,96 @@
+﻿using LiteNetLibManager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MultiplayerARPG.MMO
+{
+    public partial class MapNetworkManager
+    {
+        [Header("Sample - CustomFromClientToCentralServer")]
+        public string sampleMessageToCentralServer = "QWERTY";
+        private Coroutine sampleMessageToCentralCoroutine;
+
+        [DevExtMethods("Init")]
+        private void CustomFromClientToCentralServer_Init()
+        {
+            // Start coroutine to send message every seconds
+            if (sampleMessageToCentralCoroutine != null)
+                StopCoroutine(sampleMessageToCentralCoroutine);
+            sampleMessageToCentralCoroutine = StartCoroutine(SampleMessageToCentralRoutine());
+        }
+
+        IEnumerator SampleMessageToCentralRoutine()
+        {
+            while (true)
+            {
+                if (IsClientConnected)
+                {
+                    // Send message from client to map-server
+                    float clientTime = ServerTime;
+                    Debug.Log("[CustomFromClientToCentralServer] client send: " + sampleMessageToCentralServer + " " + clientTime);
+                    ClientSendPacket(LiteNetLib.DeliveryMethod.ReliableOrdered, 10001, (writer) =>
+                    {
+                        writer.Put(sampleMessageToCentralServer);
+                        writer.Put(ServerTime);
+                    });
+                }
+                yield return new WaitForSeconds(1);
+            }
+        }
+
+        [DevExtMethods("RegisterClientMessages")]
+        private void CustomFromClientToCentralServer_RegisterClientMessages()
+        {
+            // Clients will receive this message from server
+            RegisterClientMessage(10001, SampleHandleMsgFromMapServer);
+        }
+
+        [DevExtMethods("RegisterServerMessages")]
+        private void CustomFromClientToCentralServer_RegisterServerMessages()
+        {
+            // In map-server there is client which connect to central-server, the field is `CentralAppServerRegister`
+            // So developer may register `CentralAppServerRegister` messages here
+            CentralAppServerRegister.RegisterMessage(10001, SampleHandleMsgFromCentralServer);
+
+            // Server will receive this message from clients, then send it to central server by `CentralAppServerRegister`
+            RegisterServerMessage(10001, SampleHandleMsgFromClient);
+        }
+
+        // Client message handlers
+        private void SampleHandleMsgFromMapServer(LiteNetLibMessageHandler messageHandler)
+        {
+            Debug.Log("[CustomFromClientToCentralServer] client receive: " + messageHandler.reader.GetString() + " " + messageHandler.reader.GetFloat());
+        }
+
+        // CentralAppServerRegister message handlers
+        private void SampleHandleMsgFromCentralServer(LiteNetLibMessageHandler messageHandler)
+        {
+            string text = messageHandler.reader.GetString();
+            float clientTime = messageHandler.reader.GetFloat();
+            long connectionId = messageHandler.reader.GetLong();
+            Debug.Log("[CustomFromClientToCentralServer] map-server receive from central-server: " + text + " " + clientTime + " then send this to client");
+            ServerSendPacket(connectionId, LiteNetLib.DeliveryMethod.ReliableOrdered, 10001, (writer) =>
+            {
+                writer.Put(text);
+                writer.Put(ServerTime);
+            });
+        }
+
+        // Server message handlers
+        private void SampleHandleMsgFromClient(LiteNetLibMessageHandler messageHandler)
+        {
+            // Receive message from client at map-server
+            // Then keep transport handler at server to use later to send message to client
+            string text = messageHandler.reader.GetString();
+            float clientTime = messageHandler.reader.GetFloat();
+            Debug.Log("[CustomFromClientToCentralServer] map-server receive from client: " + text + " " + clientTime + " then send this to central-server");
+            CentralAppServerRegister.ClientSendPacket(LiteNetLib.DeliveryMethod.ReliableOrdered, 10001, (writer) =>
+            {
+                writer.Put(sampleMessageToCentralServer);
+                writer.Put(ServerTime);
+                writer.Put(messageHandler.connectionId);
+            });
+        }
+    }
+}
